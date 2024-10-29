03886da11657adb76dc6e5fcce139b531fea9f4b
Fox Tail <foxtail25@gmail.com>
2024-10-29 16:11:06 +0300

    Композиция из компонентов

--- script.js
+++ script.js
@@ -79,18 +79,24 @@ const ListRendering = {
 Vue.createApp(ListRendering).mount('#list-rendering')
 //***************************************************************************** */
 
-// const TodoItem = {
-//     props: ['todo'],
-//     template: `<li>Это одна из задач</li>`
-//   }
-  
-//   // Создаём Vue-приложение
-//   const app = Vue.createApp({
-//     components: {
-//       TodoItem // Регистрируем новый компонент
-//     },
-//     ... // Остальные свойства для компонента
-//   })
-  
-//   // Монтируем приложение Vue
-//   app.mount(...)
+const ComponentsApp = {
+  data() {
+    return {
+      groceryList: [
+        { id: 0, text: 'Vegetables' },
+        { id: 1, text: 'Cheese' },
+        { id: 2, text: 'Whatever else humans are supposed to eat' }
+      ]
+    }
+  }
+}
+
+const app = Vue.createApp(ComponentsApp)
+
+app.component('todo-item', {
+  props: ['todo'],
+  template: `<li>{{ todo.text }}</li>`
+})
+
+app.mount('#components-app')
+
